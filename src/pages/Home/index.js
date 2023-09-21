b7fbe8a22d21a5916c4104a8a0ca242d5e74163a
WevertonSPWOS <131711547+WevertonSPWOS@users.noreply.github.com>
2023-09-21 15:37:37 -0300

    Add files via upload

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,6 @@ import React           from "react";
 import Cotacao  from "../../components/cotacao";    
 import EstruturaPagina from "../../components/EstruturaPagina";
 import "./styles.css";
-import { Grafico } from "../../components/grafico";
 
 export default function Home(){
     return(
